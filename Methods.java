1966c03ae2c3b24ce40897e199f2dba303b5fa84
LucasChrispim <73845967+LucasChrispim@users.noreply.github.com>
2020-11-17 13:32:17 +0000

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package methods;
+package methods;// JUST DOING A TEST FOR THIS AMAZING CLASS
 
 /**
  *
