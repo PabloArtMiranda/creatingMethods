c2b1083d63e67f011375465a4769f387c3084327
Pablo Miranda <72030534+PabloArtMiranda@users.noreply.github.com>
2020-11-17 13:23:30 +0000

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methods;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Methods {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static void pablomethod(){
+        //Author: Pablo Miranda
+        //
+        System.out.println("This is just a method test1");
+    }
+        
+    static void dougmethod(){
+        //Author: Doug
+        //
+        System.out.println("This is just a methods test2");    
+        
+    }
+    static void jonatanmethod(){
+        //Author: Jonatan
+        //
+        System.out.println("This is just a method test3");
+    }    
+    static void lucasmethod(){
+        //Author: Lucas
+        //
+        System.out.println("This is just a method test4"); 
+    }    
+    static void Yanmethod(){
+        //Author: Yan
+        //
+        System.out.println("This is just a method test5");
+    }    
+    static void harveymethod(){
+        //Author: Harvey
+        //
+        System.out.println("This is just a method test6");    
+        
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        pablomethod();
+        dougmethod();
+        jonatanmethod();
+        lucasmethod();
+        Yanmethod();
+        harveymethod();
+    }
+    
+}
